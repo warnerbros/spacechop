8b269ccd2eea2bf451b16399f37fac519a8d9fcf
Richard Nienaber <rjnienaber@gmail.com>
2019-04-01 14:35:33 +0100

    separate out app to allow for hooks into middleware pipeline

--- src/app.ts
+++ src/app.ts
@@ -8,32 +8,39 @@ const {
   PROMETHEUS_METRIC_PATH = '/_health',
 } = process.env;
 
-// read initial config.
-let config = loadConfig();
-// create server.
-export const app = express();
-app.disable('x-powered-by');
+export function setupApp(app) {
+  // read initial config.
+  let config = loadConfig();
+  app.disable('x-powered-by');
 
-// create and setup router.
-let router = express.Router();
-// Setup routes for the SpaceChop service.
-setupRoutes(config, router, monitor);
-// Enable reloading of routes runtime by using a simple router that we switch out.
-app.use((req, res, next) => {
-  // pass through requests to the router.
-  router(req, res, next);
-});
-
-// Re-Initialize routes when new config is loaded.
-chokidar.watch('/config.yml', {
-  usePolling: true,
-  interval: 1000,
-}).on('all', async () => {
-  router = express.Router();
-  config = loadConfig();
+  // create and setup router.
+  let router = express.Router();
+  // Setup routes for the SpaceChop service.
   setupRoutes(config, router, monitor);
-});
+  // Enable reloading of routes runtime by using a simple router that we switch out.
+  app.use((req, res, next) => {
+    // pass through requests to the router.
+    router(req, res, next);
+  });
 
-app.get(PROMETHEUS_METRIC_PATH, (_, res) => {
-  res.end(monitor.getMetrics());
-});
+  // Re-Initialize routes when new config is loaded.
+  chokidar.watch('/config.yml', {
+    usePolling: true,
+    interval: 1000,
+  }).on('all', async () => {
+    router = express.Router();
+    config = loadConfig();
+    setupRoutes(config, router, monitor);
+  });
+
+  app.get(PROMETHEUS_METRIC_PATH, (_, res) => {
+    res.end(monitor.getMetrics());
+  });
+}
+
+export function createApp() {
+  // create server.
+  const app = express();
+  setupApp(app);
+  return app;
+}
